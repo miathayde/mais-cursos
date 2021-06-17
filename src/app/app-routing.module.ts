e0019e53b8c43319aefb553a0bc40d6118f6b41d
Michelle Athayde <michelle.estevinho@hotmail.com>
2021-06-16 23:27:32 -0300

    Inicio criação tabela

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from 'src/cadastro/cadastro.component';
+import { ListaCursosComponent } from 'src/lista-cursos/lista-cursos.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'home', pathMatch: 'full' },
   // { path: 'home', component: HomeComponent }
-  { path: 'cadastro', component: CadastroComponent }
+  { path: 'cadastro', component: CadastroComponent },
+  { path: '', component: ListaCursosComponent }
 ];
 
 @NgModule({
