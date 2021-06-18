40850d92fc7c114b8a1b2c6e4bd3a04cc380010d
Michelle Athayde <michelle.estevinho@hotmail.com>
2021-06-17 21:41:44 -0300

    Componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from '../header/header.component';
 import { CadastroComponent } from '../cadastro/cadastro.component';
 import { FormsModule } from '@angular/forms';
 import { ListaCursosComponent } from '../lista-cursos/lista-cursos.component';
+import { HomeComponent } from '../home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CadastroComponent,
-    ListaCursosComponent
+    ListaCursosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
